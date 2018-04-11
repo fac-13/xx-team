a8526e3e6d9a5b1d0393e45ba8e809000a97bfb1
Jennah <contactjennah@gmail.com>
2018-04-11 16:12:57 +0100

    Add client side password validation for sign up relates #7

--- public/dom.js
+++ public/dom.js
@@ -1,9 +1,31 @@
-var loginBtn = document.querySelector('#login-btn'); 
+var loginBtn = document.querySelector('#login-btn');
 var signUp = document.querySelector('#signup');
 var newUser = document.querySelector('#new-user');
+var password = document.querySelector('#password');
+var confirmPassword = document.querySelector('#confirm-password');
 
 // Show the correct dialog
-newUser.addEventListener('click', function() {
+newUser.addEventListener('click', function () {
     login.removeAttribute('open');
     signUp.setAttribute('open', 'true');
 })
+
+// Set custom validity message for singup password 
+password.addEventListener("input", function (event) {
+    if (!password.validity.valid) {
+        password.setCustomValidity("Password must contain 1 uppercase letter, a number or special character and be at least 8 characters long");
+    } else {
+        password.setCustomValidity("");
+    }
+});
+
+// Ensure that password and confirm passwords match
+confirmPassword.addEventListener("input", function (event) {
+    if (!confirmPassword.validity.valid) {
+        confirmPassword.setCustomValidity("Password must contain 1 uppercase letter, a number or special character and be at least 8 characters long");
+    } else if (confirmPassword.value !== password.value) {
+        confirmPassword.setCustomValidity("Passwords must match");
+    } else {
+        password.setCustomValidity("");
+    }
+});
