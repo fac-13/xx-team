a87ed9051edfb8951ff93b1db0f3ed7b38ff0cd8
tspeed90 <tspeed90@gmail.com>
2018-04-12 10:46:29 +0100

    adds validation for email and password fields -- CONFIRMING BROKEN

--- public/dom.js
+++ public/dom.js
@@ -1,8 +1,10 @@
 var loginBtn = document.querySelector('#login-btn');
+var signupBtn = document.querySelector('#signup-btn')
 var signUp = document.querySelector('#signup');
 var newUser = document.querySelector('#new-user');
 var password = document.querySelector('#password');
 var confirmPassword = document.querySelector('#confirm-password');
+
 
 // Show the correct dialog
 newUser.addEventListener('click', function () {
@@ -12,20 +14,20 @@ newUser.addEventListener('click', function () {
 
 // Set custom validity message for singup password 
 password.addEventListener("input", function (event) {
-    if (!password.validity.valid) {
+    if (!/(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![\n])(?=.*[A-Z])(?=.*[a-z]).*$/.test(password.value)) {
         password.setCustomValidity("Password must contain 1 uppercase letter, a number or special character and be at least 8 characters long");
     } else {
         password.setCustomValidity("");
     }
 });
 
-// Ensure that password and confirm passwords match
-confirmPassword.addEventListener("input", function (event) {
-    if (!confirmPassword.validity.valid) {
-        confirmPassword.setCustomValidity("Password must contain 1 uppercase letter, a number or special character and be at least 8 characters long");
-    } else if (confirmPassword.value !== password.value) {
+function checkPasswords(event) {
+    event.preventDefault();
+    if (confirmPassword.value !== password.value) {
         confirmPassword.setCustomValidity("Passwords must match");
-    } else {
-        password.setCustomValidity("");
     }
-});
+}
+// Ensure that password and confirm passwords match
+signupBtn.addEventListener("submit", checkPasswords); 
+
+
