37202e6c8406c3fdfb3a0c6e265297901baf2f78
tspeed90 <tspeed90@gmail.com>
2018-04-12 14:29:24 +0100

    adds xhr to get data from /viewall and display

--- public/dom.js
+++ public/dom.js
@@ -5,6 +5,53 @@ var newUser = document.querySelector('#new-user');
 var password = document.querySelector('#password');
 var confirmPassword = document.querySelector('#confirm-password');
 
+var xhr = new XMLHttpRequest();
+var makeRequest = function (url, callback, errorCallback) {
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        var data = JSON.parse(xhr.responseText);
+        callback(data);
+      } else {
+        errorCallback();
+      }
+    }
+  }
+  xhr.open('GET', url, true);
+  xhr.send();
+}
+
+makeRequest('/viewall', displayPosts, handleError);
+
+
+function displayPosts(data) {
+
+    data.forEach(function(post) {
+        var article = document.createElement('article');
+        article.className = 'post';
+        var p = document.createElement('p');
+        var text = document.createTextNode(post.comment);
+        var author = document.createElement('span');
+        author.className = 'author';
+        var email = document.createTextNode(post.email);        
+        var date = document.createElement('span');
+        date.className = 'date';
+        var timestamp = document.createTextNode('2018-05-15');
+        console.log(text, email, timestamp);
+        p.appendChild(text);
+        author.appendChild(email);
+        date.appendChild(timestamp);
+        article.appendChild(p)
+        article.appendChild(author);
+        article.appendChild(timestamp);
+        document.getElementById('posts').appendChild(article);
+    })
+}
+
+function handleError() {
+    console.log('error');
+}
+
 
 // Show the correct dialog
 newUser.addEventListener('click', function () {
