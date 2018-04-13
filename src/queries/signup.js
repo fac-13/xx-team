4526bac7eb44651b24dbc065c632b096817041bb
Parissai <parissa.jamali@gmail.com>
2018-04-13 13:41:54 +0100

    remove console.log

--- src/queries/signup.js
+++ src/queries/signup.js
@@ -5,29 +5,28 @@ const bcrypt = require('bcryptjs');
 
 
 const signup = (email, password, cb) => {
-       bcrypt.genSalt(10, (err, salt) => {
-            if (err) {
-                cb(err);
-            } else {
-                bcrypt.hash(password, salt, (err,hash)=> {
-                   if(err){
-                       cb(err)
-                   }else{
-                        console.log('talking from just b4 insert', email, password, hash);
-                        dbConnection.query(`INSERT INTO users (email, encrypted_password) VALUES ($1, $2);`, [email, hash], (err, res) => {
-                            if (err) {
-                                cb(err);
-                            } else {
-                                cb(null, res.rowCount); //redirect to log in
-                            }
-                        });
-                   }
-               });
-            }
-        });
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+            cb(err);
+        } else {
+            bcrypt.hash(password, salt, (err, hash) => {
+                if (err) {
+                    cb(err)
+                } else {
+                    dbConnection.query(`INSERT INTO users (email, encrypted_password) VALUES ($1, $2);`, [email, hash], (err, res) => {
+                        if (err) {
+                            cb(err);
+                        } else {
+                            cb(null, res.rowCount); //redirect to log in
+                        }
+                    });
+                }
+            });
+        }
+    });
 
 
 
 }
 
-module.exports={ signup };
+module.exports = { signup };
